5a5523bb5036e5bbc0b821d37410b003a1ed1e68
Baldeep-98 <Bsharma6657@conestogac.on.ca>
2024-06-21 22:21:03 -0400

    adding apiHandler in backend

---
+++ backend/apiHandler.js
@@ -0,0 +1,39 @@
+const GraphQLDate = require('./graphqlDate')
+const user = require('./users')
+const { ApolloServer } = require('apollo-server');
+const fs = require('fs');
+const book = require('./book');
+
+const resolvers = {
+    Query: {
+        getUser: user.getUser,
+        BookList: book.list,
+    },
+    Mutation: {
+        userAdd: user.userAdd,
+    },
+    
+    GraphQLDate,
+}
+
+
+const server = new ApolloServer({
+    typeDefs: fs.readFileSync('./schema.graphql', 'utf-8'), // schema
+    resolvers,
+    // just to print error in the console
+    formatError: (error) => {
+        console.log(error);
+        return error;
+    },
+});
+
+// Start the server
+
+function installHandler(PORT) {
+    server.listen({port: PORT}).then(({ url }) => {
+        console.log(`Server ready at ${url}`);
+    });
+};
+
+
+module.exports = { installHandler };
