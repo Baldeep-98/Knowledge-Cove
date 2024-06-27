6f3d77a67623f8e8ca64eccdfc3df0243138234e
Baldeep-98 <Bsharma6657@conestogac.on.ca>
2024-06-27 10:57:42 -0400

    adding complete bookDetail page functionality to frontend

--- frontend/src/components/BookDetail.jsx
+++ frontend/src/components/BookDetail.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { useQuery, gql } from '@apollo/client';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
 
-const GET_BOOK_DETAIL = gql`
+const GET_BOOK_DETAIL = gql` 
   query GetBookDetail($book_id: Int!) {
     getBook(book_id: $book_id) {
       _id
@@ -14,45 +14,48 @@ const GET_BOOK_DETAIL = gql`
       book_longDescription
       book_image_url
     }
-  }
-`;
+  }`;
 
 const BookDetail = () => {
   const { id } = useParams();
-  const bookId = parseInt(id, 10);
+  const bookId = parseInt(id);
 
   console.log(`Fetching details for book ID: ${bookId}`);
 
-  const { loading, error, data } = useQuery(GET_BOOK_DETAIL, {
-    variables: { book_id: bookId },
-  });
+  const { loading, error, data } = useQuery(GET_BOOK_DETAIL, {  variables: { book_id: bookId },});//fetching book detail based on bookId
 
   const [book, setBook] = useState(null);
 
   useEffect(() => {
-    if (data) {
-      console.log('Raw data received:', data);
+    if (data) { 
+      console.log("book received", data);
       if (data.getBook) {
-        console.log('Book data retrieved:', data.getBook);
-        setBook(data.getBook);
+        console.log("Book data retrieved:", data.getBook); 
+        setBook(data.getBook);//updating book state after book fetch
       } else {
-        console.log('No book data found for the given ID.');
+        console.log("No book data found for  ID.");
       }
     }
   }, [data]);
-  if (!book) return <p>Book not found</p>; 
+  if (!book) return <p>Book not found</p>;
   return (
     <div className="book-detail">
+       <div className="book-image-container">
+        <img
+          src={book.book_image_url}
+          alt={book.book_name}
+          className="book-image"
+        />
+      </div>
       <div className="book-info">
         <h1>{book.book_name}</h1>
-        <p>Author: {book.book_author}</p>
-        <p>Genre: {book.book_genre}</p>
-        <p>Short Description: {book.book_shortDescription}</p>
-        <p>Long Description: {book.book_longDescription}</p>
+        <p><strong>Author:</strong> {book.book_author}</p>
+        <p><strong>Genre:</strong> {book.book_genre}</p>
+        <p><strong>Short Description:</strong> {book.book_shortDescription}</p>
+        <p><strong>Long Description: </strong>{book.book_longDescription}</p>
+        <button>Add To Cart</button>
       </div>
-      <div className="book-image-container">
-        <img src={book.book_image_url} alt={book.book_name} className="book-image" />
-      </div>
+     
     </div>
   );
 };
