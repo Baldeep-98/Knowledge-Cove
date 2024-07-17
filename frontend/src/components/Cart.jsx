96b7f36a6e6ee227c024152ba48d4aa44978b2ac
Baldeep-98 <Bsharma6657@conestogac.on.ca>
2024-07-16 21:42:34 -0400

    Adding styling to cart page- Adjusting the Cart.css file make this page more styling and easy to use for user .

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useQuery, gql } from "@apollo/client";
+import { Link, useNavigate } from "react-router-dom";
 
 const GET_CART_Books = gql`
   query GetCartBooks {
@@ -23,16 +24,20 @@ const Cart = () => {
   const { data } = useQuery(GET_CART_Books); //useQuery hook to run Get_cart_books query
 
   const [CartItems, setCartItems] = useState([]); // state to hold the items of cart
-
+ const navigate=useNavigate();
   useEffect(() => {    //to update the state of cart items
 
     if (data) {
       setCartItems(data.CartItems);
     }
   }, [data]);
+  const handleClick=(book_id)=>{
+    navigate(`/checkout/${book_id}`);
+    console.log("button clicked");
+  }
   return (
-    <div className="cart">
-      <h1>Your Cart</h1>
+    <div className="cart" onClick={handleClick}>
+      <h1 className="cartHead">Shopping Cart</h1>
       {CartItems.map((item) => (
         <div key={item._id} className="cart-info">
           <div className="cart-image-container">
@@ -44,8 +49,9 @@ const Cart = () => {
           </div>
           <div className="book-info">
             <h2>{item.bookDetails.book_name}</h2>
-            <p>{item.bookDetails.book_author}</p>
-            <p>{item.bookDetails.book_genre}</p>
+            <p className="cartdesc">Book Author:{item.bookDetails.book_author}</p>
+            <p className="cartdesc">Book Genre:{item.bookDetails.book_genre}</p>
+            <Link to={`/checkOut/${item.bookDetails.book_id}`}></Link>
            <button className="btncheckout">Check Out</button>
            </div>
         </div>
